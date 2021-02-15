f98ceecd46d1a79597805a1094077a9f2070f857
Laura J <laura.jakseboge@gmail.com>
2021-02-15 16:42:10 +0100

    pagination added;

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Table, TableBody } from '@material-ui/core'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TablePagination,
+  TableRow,
+} from '@material-ui/core'
 
 import { Country, TableOfCountriesProps } from '../../types'
 import TableHeader from './Header'
@@ -10,24 +16,56 @@ export default function TableOfCountries({
   data,
   handleSort,
 }: TableOfCountriesProps) {
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(5)
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage)
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }
+
   return (
     <Table>
       <TableHeader handleSort={handleSort} />
       <TableBody>
-        {data.map((country: Country) => {
-          const { flag, name, languages, population, region } = country
-          return (
-            <Row
-              key={name}
-              flag={flag}
-              name={name}
-              languages={languages}
-              population={population}
-              region={region}
-            />
-          )
-        })}
+        {(rowsPerPage > 0
+          ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+          : data
+        ).map((country: Country) => (
+          <Row
+            key={country.name}
+            flag={country.flag}
+            name={country.name}
+            languages={country.languages}
+            population={country.population}
+            region={country.region}
+          />
+        ))}
+        {emptyRows > 0 && (
+          <TableRow style={{ height: 53 * emptyRows }}>
+            <TableCell colSpan={6} />
+          </TableRow>
+        )}
       </TableBody>
+      <TablePagination
+        count={data.length}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+        rowsPerPage={rowsPerPage}
+        page={page}
+      />
     </Table>
   )
 }
